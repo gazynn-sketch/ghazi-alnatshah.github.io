66f4f5a20f2edd60a5e61e05dcf90cdc8c5656b6
gazynn-sketch <gazynn@gmail.com>
2026-08-06 14:01:39 +0300

    Add family notifications backend configuration

---
+++ notifications-config.js
@@ -0,0 +1,6 @@
+window.NATSHA_NOTICE_CONFIG = Object.freeze({
+  apiUrl: '',
+  spreadsheetId: '1eDulzaGE3GRrfky_yq6p8yzxS45SJWl-qz5IgmKZbSE',
+  ownerEmail: 'gazynn@gmail.com',
+  whatsappEnabled: false
+});
