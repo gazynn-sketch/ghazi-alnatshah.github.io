089edfde19f3d0ed8dbd917bf6bb05fddb0517ab
gazynn-sketch <gazynn@gmail.com>
2026-08-06 15:05:34 +0300

    Connect live family notifications Apps Script endpoint

--- notifications-config.js
+++ notifications-config.js
@@ -1,5 +1,5 @@
 window.NATSHA_NOTICE_CONFIG = Object.freeze({
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwq2Hxvg6aKX8hGD8vOfbX2uW1Kq7U2_1MxhE8dS6dmOmscoQU4zmiU_QJvfwN5YHuP/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwqY9A2JlUEXxUJrTcMegJ0Qwy1cOn9jGiKb8E9ObWdkYy6jOlfROR0Kmx9G68q8N1H/exec',
   spreadsheetId: '1eDulzaGE3GRrfky_yq6p8yzxS45SJWl-qz5IgmKZbSE',
   ownerEmail: 'gazynn@gmail.com',
   whatsappEnabled: false
