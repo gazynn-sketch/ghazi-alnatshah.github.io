5ff24b19460e212b27d16d1b7f45881fd224decb
gazynn-sketch <gazynn@gmail.com>
2026-08-08 11:50:55 +0300

    Enable direct Firebase push in admin panel

--- notifications-config.js
+++ notifications-config.js
@@ -2,5 +2,6 @@ window.NATSHA_NOTICE_CONFIG = Object.freeze({
   apiUrl: 'https://script.google.com/macros/s/AKfycbwqY9A2JlUEXxUJrTcMegJ0Qwy1cOn9jGiKb8E9ObWdkYy6jOlfROR0Kmx9G68q8N1H/exec',
   spreadsheetId: '1eDulzaGE3GRrfky_yq6p8yzxS45SJWl-qz5IgmKZbSE',
   ownerEmail: 'gazynn@gmail.com',
-  whatsappEnabled: false
+  whatsappEnabled: false,
+  pushEnabled: true
 });
